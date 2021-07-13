585f4276e2c2ea95667b013ed57b06cfd6b491fd
luisfelipe <luis.felipeszsv@gmail.com>
2021-07-13 00:23:50 -0300

    making enviroment consts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,26 +3,16 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCKiI6NFtFdbl1rFC0redvxdrLySQ6aX68",
-  authDomain: "letmeask-ef5a1.firebaseapp.com",
-  projectId: "letmeask-ef5a1",
-  storageBucket: "letmeask-ef5a1.appspot.com",
-  messagingSenderId: "768431590938",
-  appId: "1:768431590938:web:c43710373aecfa0f2a4ba3",
-  measurementId: "G-HT9JK4WGDH"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSASING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCKiI6NFtFdbl1rFC0redvxdrLySQ6aX68",
-//   authDomain: "letmeask-ef5a1.firebaseapp.com",
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSASING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-// };
-
 
 firebase.initializeApp(firebaseConfig)
 
