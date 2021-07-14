555caf2fc947327f702f70737343288398adccb2
luisfelipe <luis.felipeszsv@gmail.com>
2021-07-13 23:31:16 -0300

    created base room page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -9,7 +10,6 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
-import { database } from '../services/firebase'
 
 export function Home() {
     const history = useHistory()
